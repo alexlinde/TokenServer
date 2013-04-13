5383e6d98b534c0e7a8dba9710c62cf7683d6318
Alex Linde <alex@somefamilies.com>
2013-04-13 10:58:14 -0700

    added socket.io

--- web.js
+++ web.js
@@ -1,12 +1,29 @@
-var express = require("express");
-var app = express();
+var app = require('express')();
 app.use(express.logger());
-
-app.get('/', function(request, response) {
-        response.send('Hello World!');
-        });
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
            console.log("Listening on " + port);
            });
+
+// serve the homepage for iphone management
+app.get('/', function (req, res) {
+        res.sendfile(__dirname + '/index.html');
+        });
+
+// heroku does not support websockets so force polling
+io.configure(function () {
+             io.set("transports", ["xhr-polling"]);
+             io.set("polling duration", 10);
+             });
+
+// handle the socket
+io.sockets.on('connection', function (socket) {
+              socket.emit('news', { hello: 'world' });
+              socket.on('my other event', function (data) {
+                        console.log(data);
+                        });
+              });
+
